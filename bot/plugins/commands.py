98536811d86209b8002dc26a732e0e480e5bc5bd
SawinKunjumon <86658940+SawinKunjumon@users.noreply.github.com>
2021-07-26 17:32:10 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @cochinmovies",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰 Our Group 🔰', url="https://t.me/cochinmovies"
                                 )
                         ]
                     ]
